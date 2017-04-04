515bba4d61f1fc8ebc395ce973cc96a1a608f7cd
Stelyz <natanfer@live.com>
2017-04-04 09:32:02 -0300

    Update TrabalhoModificado5.sh

--- TrabalhoModificado5.sh
+++ TrabalhoModificado5.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+CISCO=1; NAT=0; MAT=0; THI=0  clear
+while (( $CISCO != 0 )); do
+echo "Digite um salario"
+read CISCO; SALARIO=$CISCO
+[ $SALARIO == $NAT ] && let SALARIO=(THI-1)
+[ $SALARIO == $MAT ] && let SALARIO=(THI-1)
+if (( $SALARIO > $NAT )); then
+		THI=$MAT
+		MAT=$NAT
+		NAT=$SALARIO
+else
+	if  (( $SALARIO > $MAT )); then
+			THI=$MAT
+			MAT=$SALARIO
+	else
+
+		if (( $SALARIO > $THI )); then
+			THI=$SALARIO
+
+		fi
+	fi
+fi
+done
+echo "Os 3 maiores salarios são respectivamente:$NAT,$MAT,$THI"
